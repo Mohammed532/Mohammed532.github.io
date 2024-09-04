5fbde8f142f193176862420774cc87e92df3e029
Mohammed Akinbayo <mo@Mohammeds-MacBook-Pro.local>
2024-09-03 20:54:30 -0400

    Ok think I got the bug this time

--- src/app/_hooks/useWindowDimensions.ts
+++ src/app/_hooks/useWindowDimensions.ts
@@ -1,18 +1,22 @@
+'use client'
+
 import { useState, useEffect } from 'react';
 
 // TODO: fix 'window is not defined' error
 export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
-
-  function getWindowDimensions() {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-      width,
-      height
-    };
-  }
+  const [windowDimensions, setWindowDimensions] = useState({width: 0, height: 0});
 
   useEffect(() => {
+    function getWindowDimensions() {
+      const { innerWidth: width, innerHeight: height } = window;
+      return {
+        width,
+        height
+      };
+    }
+
+    setWindowDimensions(getWindowDimensions)
+
     function handleResize() {
       setWindowDimensions(getWindowDimensions());
     }
