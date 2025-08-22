2fa8c73c997fd28eec21d7efb7b00e77c5b4c77f
Mohammed Akinbayo <35383252+Mohammed532@users.noreply.github.com>
2025-08-22 04:01:08 -0400

    Update footer.tsx

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -5,7 +5,7 @@ import { FiGithub, FiLinkedin } from "react-icons/fi"
 export default function Footer() {
     return (
         <footer className="m-10">
-            <h1 className="text-center mt-40 mb-20 text-4xl">Stay Blessed :)</h1>
+{/*             <h1 className="text-center mt-40 mb-20 text-4xl">Stay Blessed :)</h1> */}
             <div className="flex flex-col justify-items-center content-center gap-4 md:justify-stretch md:flex-row">
                 <p className="self-center italic text-center md:text-left grow">Made By Mohammed Akinbayo (Duh)</p>
                 <div className="flex place-self-center justify-self-center justify-items-center order-last md:order-none grow">
