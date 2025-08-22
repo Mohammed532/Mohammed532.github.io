6d905b35188cfb75365d36d7df2b52f081ba7f93
Mohammed Akinbayo <mo@mohammeds-mbp.mynetworksettings.com>
2025-08-22 04:28:16 -0400

    Fixed mobile timeline bug

--- src/app/_landing_components/Timeline.tsx
+++ src/app/_landing_components/Timeline.tsx
@@ -186,9 +186,12 @@ function Error(){
     )
 }
 
+// TODO: update for Firebase, simply returns input string for now
 function ISOtoCustomDateString(isoString: string){
     const months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
 
-    const d = new Date(isoString);
-    return `${months[d.getMonth()]} ${d.getFullYear()}`
+    // const d = new Date(isoString);
+    // return `${months[d.getMonth()]} ${d.getFullYear()}`
+
+    return isoString;
 }
